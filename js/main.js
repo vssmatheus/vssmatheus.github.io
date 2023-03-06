96479b1f321483fac787e4165e426c1b7e3e7c0e
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2023-03-06 00:24:58 -0300

    start do projeto (#7)

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const myFunction = () => {
-    var copyText = document.getElementById("emailItem");
-
-    copyText.click();
-    navigator.clipboard.writeText(copyText.value);
-
-    alert("Email copied: " + copyText.value);
-}
-
